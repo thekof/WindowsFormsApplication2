5de8a432c59f74e33cba19fcba36aaef458e923f
vitoco89 <vitoco2489@gmail.com>
2014-02-21 12:12:55 -0300

    Vitoco89
    Cambios

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // web
+        }
     }
 }
